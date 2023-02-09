454cd7efadbe2e7e0a6cbaccec3189bdfe8df5ba
Breno Dias Santana Silva <118858385+bdsilva16@users.noreply.github.com>
2023-02-09 13:18:15 +0000

    novas atualizações

--- src/pages/home/components.tsx
+++ src/pages/home/components.tsx
@@ -20,4 +20,23 @@ export const Pokemon = styled.li`
     display: flex;
     flex-direction: column;
     align-items: center;
+
+    img{
+        width: 180px;
+        border-radiux: 1rem;
+        margin-botton: 2rem;
+    }
+
+    span{
+        font-weight: bold;
+        font-size: 120%;
+        text-align: center;
+    }
+
+    a{
+        transition: all 0.4s;
+    }
+    a:hover {
+        transform: scale(1.1);
+    }
 `
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,27 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { moveEmitHelpers } from "typescript";
 import { Container, PokeList, Pokemon } from "./components";
 
 export default function Home(){
+
+    //const [pokemons, setPokemons] = useState<any[]>([])
+
+    const pokemons = [
+        {
+            title:"new",
+            poster_path:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png"
+        }
+    ]
+
     return(
         <Container>
             <h1>Minha Pokedex</h1>
             <PokeList>
-                <Pokemon>
-                    <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png" alt="new"/>
-                    <span>new</span>
-                </Pokemon>
-            </PokeList>
-            <PokeList>
-                <Pokemon>
-                    <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png" alt="new"/>
-                    <span>new</span>
-                </Pokemon>
-            </PokeList>    
-            <PokeList>
-                <Pokemon>
-                    <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png" alt="new"/>
-                    <span>new</span>
-                </Pokemon>
+                {
+                    pokemons.map(pokemon => {
+                        return(
+                           <Pokemon>
+                                <a href="#"><img src={pokemon.poster_path} alt={pokemon.title}/></a>
+                                <span>{pokemon.title}</span>
+                
+                            </Pokemon> 
+                        )
+                    })
+                }
             </PokeList>                    
         </Container>
     );
